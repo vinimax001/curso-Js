bce849203fb36695f3f3e25ebf6684df0f0517f2
vinimax <vini.ciusang@gmail.com>
2025-02-16 16:30:39 -0300

    added class 05 - simple conditions

---
+++ aula05/ex008.js
@@ -0,0 +1,6 @@
+var vel = 75.5
+console.log(`A velocimdade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //Condição simples!
+    console.log(`Você ultrapassou o limite de velocidade permitida. MULTADO.`)
+}
+console.log('Dirija sempre usando cinto de segunrça!')
---
+++ aula05/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Você nasceu no ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
