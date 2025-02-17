865649b6e6eaca970ccb0edc6bdd012ce083141e
vinimax <vini.ciusang@gmail.com>
2025-02-17 10:48:25 -0300

    finished ex014

--- aula12ex/ex014/assets/script.js
+++ aula12ex/ex014/assets/script.js
@@ -1,7 +1,22 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('div#foto')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML =`Agora são ${hora} horas.`
+    var img = window.document.getElementById('imagem')
+    data = new Date()
+    hora = data.getHours()
+    //hora = 18    
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12){
+        //Bom Dia 
+        img.src = 'assets/images/manha.png'
+        window.document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        //Boa Tarde
+        img.src = 'assets/images/tarde.png'
+        window.document.body.style.background = '#b9846f'
+    } else {
+        //Boa Noite
+        img.src = 'assets/images/noite.png'
+        window.document.body.style.background = '#515154'        
+    }
+
 }
