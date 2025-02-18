29549c4b06b5d0aad3462e158ea6bff6090ed682
vinimax <vini.ciusang@gmail.com>
2025-02-18 15:23:07 -0300

    repeating structures

---
+++ aula13/dowhile.js
@@ -0,0 +1,10 @@
+//Repetições com teste lógico no final
+
+//Faça (do) enquanto (while)
+var c = 10
+/*
+do{
+    console.log(`${c}ºVez`)
+    c--
+} while(c >= 1)
+*/
---
+++ aula13/while.js
@@ -0,0 +1,34 @@
+//Contando até 10 crescente e decrescente com variaveis de repetição (while)
+
+//Enquanto (while)
+//colocando o cont antes do log utilizar <=
+
+//decrescente
+
+var conta = 10
+while (conta >= 1){
+    console.log(`Passo ${conta}`)
+    conta -- // conta recebe cont =+ 1
+}
+
+
+var con = 10
+while (con >= 1){     
+    console.log(`volta ${con}`)
+    con-- //- 1 a cada repetição
+}
+
+
+
+//Crescente
+var c = 1
+while (c <= 10){    
+    console.log(`log ${c}`)
+    c ++ // cont recebe cont =+ 1 
+}
+
+var cont = 1
+while (cont <= 10){    
+    console.log(`${cont}° Vez`)
+    cont++
+}
