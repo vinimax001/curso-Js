e6887bf3e24f7f118c7750e09ed0d93dc130ce6a
vinimax <vini.ciusang@gmail.com>
2025-02-19 13:06:59 -0300

    Class 14 exercises completed

--- aula14ex/ex017/assets/script.js
+++ aula14ex/ex017/assets/script.js
@@ -1,9 +1,24 @@
-function calcular(){
-var operador = window.document.getElementById('operador')
-var op = Number(operador.value)
-var res = window.document.querySelector('div#res')
-for(cont = 1; cont <= 10; cont ++) {
-    var resultado = op * cont
-    res.innerHTML += `${op} X ${cont} = ${resultado}<br>`
-} 
+function tabuada(){
+
+//Inicializando variaveis    
+    let num = window.document.querySelector('#txtn')
+    let tab = window.document.querySelector('#seltab')
+
+//Convertendo para número e verificando valores nulos
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')    
+    } else {
+        let op = Number(num.value)
+        
+//Limpando a área do select
+        tab.innerHTML = ''
+
+//Gerando tabuada
+        for(cont = 1; cont <= 10; cont++) {
+            let item = document.createElement('option')
+            let res = op * cont
+            item.text =`${op} x ${cont} = ${res}`
+            tab.appendChild(item)
+        }
+    }
 }
