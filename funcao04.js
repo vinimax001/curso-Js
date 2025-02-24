44d83cf04d5d5a18795422ef4169eea004287e28
vinimax <vini.ciusang@gmail.com>
2025-02-24 13:48:33 -0300

    class funcions

---
+++ funcao04.js
@@ -0,0 +1,11 @@
+// 5 fatorial 
+// 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+function fatorial(n) {
+    let fat = 1
+    for(c =n; c > 1; c--){
+        fat *= c
+    }
+    return fat
+}
+console.log(fatorial(5))
